bee1bf0a787840ced0e2d7dc79e2fbebd08b5d65
TmaxSco <2771593655@qq.com>
2017-12-20 16:54:16 +0800

    调整初始化速度

--- Action_User/ctrl.c
+++ Action_User/ctrl.c
@@ -295,7 +295,7 @@ void HomingMode(void)
 
 	output = VelCtrl(Driver.HomingMode.Vel);
 	
-	Driver.HomingMode.Output = MaxMinLimit(output,1.5f);//限制home模式时电流大小
+	Driver.HomingMode.Output = MaxMinLimit(output,1.5f);//限制home模式时电流值
 	
 	if(fabsf(Driver.PosCtrl.ActualPos - posLast) <=2){		//2
 		Driver.HomingMode.Cnt++;
